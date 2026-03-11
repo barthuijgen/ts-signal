c8147ca9798eb54d8ab4329e26c48ce79994fa4a
Bart Huijgen <barthuijgen@users.noreply.github.com>
2026-03-11 20:46:30 +0100

    feat: add back minified build since cdns actually use them

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,13 +1,36 @@
 import { defineConfig } from "rolldown";
 import { dts } from "rolldown-plugin-dts";
 
-export default defineConfig({
-  input: "src/index.ts",
-  output: [
-    {
-      dir: "dist",
-      format: "es",
-    },
-  ],
-  plugins: [dts()],
-});
+export default defineConfig([
+  // Regular build with `index.d.ts` types
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        dir: "dist",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      dts({
+        compilerOptions: {
+          declaration: true,
+          declarationMap: true,
+        },
+      }),
+    ],
+  },
+  // Minified build `index.min.js` for cdn usage
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.min.js",
+        format: "es",
+        sourcemap: true,
+        minify: true,
+      },
+    ],
+  },
+]);
